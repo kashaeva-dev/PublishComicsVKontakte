6ceaea76a38155aa33b5778978c3aeac0c44ad4d
Nataliya Kashaeva <n.a.kashaeva@gmail.com>
2023-04-28 13:36:02 +0300

    Add function to get a url for photos' wall upload server in VK

--- publish_comics_vk.py
+++ publish_comics_vk.py
@@ -1,5 +1,6 @@
 import requests
 import os
+from environs import Env
 
 def get_image(url, filename):
 
@@ -24,3 +25,30 @@ def get_xkcd_comics(comics_number):
     get_image(url, filename)
 
     return filename, message
+
+
+def get_wall_upload_server_vk(access_token, group_id, version):
+
+    url = 'https://api.vk.com/method/photos.getWallUploadServer'
+
+    params = {
+        'access_token': access_token,
+        'v': version,
+        'group_id': group_id,
+    }
+
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+
+    upload_url = response.json()['response']['upload_url']
+
+    return upload_url
+
+
+if __name__ == '__main__':
+    env = Env()
+    env.read_env()
+    access_token = env('VK_TEST_5_TASK_ACCESS_TOKEN')
+    group_id = env('VK_PUBLISH_XKCD_COMICS_GROUP_ID')
+    version = env('VK_API_VERSION')
+    print(get_wall_upload_server_vk(access_token, group_id, version))
